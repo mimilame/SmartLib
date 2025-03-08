9461c497233e196db239feae16d2b32d4dbe47d1
Steffi Wong <111675490+mimilame@users.noreply.github.com>
2025-03-08 16:58:03 +0800

    .

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
-import './bootstrap';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
 
-import Alpine from 'alpinejs';
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
+    return pages[`./Pages/${name}.vue`];
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el);
+  },
+});
 
-window.Alpine = Alpine;
-
-Alpine.start();
+InertiaProgress.init();
--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,8 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
-
+    
+Route::get('/inertia-test', function () {
+    return Inertia\Inertia::render('Test');
+});
 require __DIR__.'/auth.php';
